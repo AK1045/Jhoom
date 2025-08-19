98470dd66978f336217be1b29b8c5acbd4e1c7a5
AK1045 <amankhan104507@gmail.com>
2025-08-19 15:04:26 +0530

    error message

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -79,7 +79,8 @@ if (process.env.NODE_ENV === "production") {
 
 // error handler
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+  console.error("Error:", err); // log full error
+  res.status(500).json({ message: err.message });
 });
 
 httpServer.listen(PORT, () => {
